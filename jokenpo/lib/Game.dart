c24c60d4fb1daebe96124ebc8c4caa7f6ae2560a
Gilberto Silva <betosilva.luz@gmail.com>
2022-04-30 15:17:17 -0300

    Criando layout.

--- jokenpo/lib/Game.dart
+++ jokenpo/lib/Game.dart
@@ -15,8 +15,44 @@ class _GameState extends State<Game> {
           title: const Text("Jokenpo")
       ),
       body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            border: Border.all(width: 3, color: Colors.yellow)
+        ),
         child: Column(
-          children: [],
+          children: <Widget>[
+            const Padding(
+                 padding: EdgeInsets.only(top: 32, bottom: 16),
+               child: Text(
+                 "Escolha do App",
+                 textAlign: TextAlign.center,
+                 style: TextStyle(
+                     fontSize: 20,
+                     fontWeight: FontWeight.bold
+                 ),
+               ),
+             ),
+            Image.asset("images/padrao.png"),
+            const  Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 16),
+              child: Text(
+                "Escolha uma opção abaixo",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset("images/pedra.png", height: 100,),
+                Image.asset("images/papel.png", height: 100),
+                Image.asset("images/tesoura.png", height: 100)
+              ],
+            )
+          ],
         ),
       ),
     );
